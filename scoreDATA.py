14d7c4b493d9100847d7c5a3d951fc469b6de947
Andrew McDonald <77781807+poisso@users.noreply.github.com>
2021-12-10 18:25:17 +0100

    create scoreDATA file

---
+++ scoreDATA.py
@@ -0,0 +1,15 @@
+def score(data, m):
+    mx = max(data)
+    tabdata = [0 for _ in range(mx)]
+    for i in range(mx):
+        tabdata[i] = len(np.where(np.array(data) == i))
+
+    dist = generateLD(m, mx)
+    score = sum(-np.log(np.array(dist)**tabdata))
+
+    return score
+    
+df1 = pd.read_csv('mutantcount1', header=None)
+m = np.median(df1)
+data = df1.to_numpy().flatten()
+print(score(data, m))
