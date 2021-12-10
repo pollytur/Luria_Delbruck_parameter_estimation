8d7c57d10b76ba6be206bcbdbc6d29b4300fc30b
Andrew McDonald <77781807+poisso@users.noreply.github.com>
2021-12-10 18:24:22 +0100

    create generateLD file

---
+++ generateLD.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def generateLD(m, max):
+    ldDist = [np.exp(-m)]
+    for i in range(1, max):
+        x = np.array([i for i in range(len(ldDist))])
+        temp = m/len(x)*sum(ldDist/(len(x) - x + 1))
+        ldDist.append(temp)
+    return ldDist
+  
+max = df1[0].max()
+distrib = generateLD(np.median(df1), max)
+
+plt.plot(distrib)
+plt.xlabel('number of mutant cells')
+plt.ylabel('probability of event')
+plt.title("graph of the Luria Delbruck distribution for the file mutantcount1")
+plt.show()
