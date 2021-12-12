ed0179387fb912d70b218983b27207203b11118f
PollyTur <p.turischeva@innopolis.university>
2021-12-12 16:37:03 +0100

    2 params evaluation

--- generateLD.py
+++ generateLD.py
@@ -1,21 +1,29 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+from math import factorial
+
 
 def generateLD(m, max):
     ldDist = [np.exp(-m)]
     for i in range(1, max):
         x = np.array([i for i in range(len(ldDist))])
-        temp = m/len(x)*sum(ldDist/(len(x) - x + 1))
+        temp = m / len(x) * sum(ldDist / (len(x) - x + 1))
         ldDist.append(temp)
     return ldDist
 
-# df1 = pd.read_csv('mutantcount1', header=None)
-# max = df1[0].max()
-# distrib = generateLD(np.median(df1), max)
 
-# plt.plot(distrib)
-# plt.xlabel('number of mutant cells')
-# plt.ylabel('probability of event')
-# plt.title("graph of the Luria Delbruck distribution for the file mutantcount1")
-# plt.show()
+def generate_poisson(lamb, max):
+    dist = [0] * (max + 1)
+    for i in range(max + 1):
+        dist[i] = (lamb ** i) * np.exp((-1) * lamb) / factorial(i)
+    return dist
+
+
+def generate_two_params(m, d, max):
+    ld = np.asarray(generateLD(m, max))
+    pois = np.asarray(generate_poisson(m * d, max))
+    pois[np.isnan(pois)] = 0
+    codist = []
+    for i in range(max + 1):
+        mult = ld[: i + 1] @ np.fliplr(pois[:i + 1])
+        codist.append(mult)
+    return codist
--- scoreDATA.py
+++ scoreDATA.py
@@ -1,19 +1,22 @@
-import numpy as np
-import pandas as pd
-from generateLD import generateLD
+from generateLD import *
 
-def score(data, m):
+
+def score(data, m, single=True, d=None):
+    '''
+    :param data:
+    :param m:
+    :param single:
+    :param d:
+    :return:
+    '''
     mx = max(data)
     tabdata = [0 for _ in range(mx)]
     for i in range(mx):
         tabdata[i] = len(np.where(np.array(data) == i))
-
-    dist = generateLD(m, mx)
-    score = sum(-np.log(np.array(dist)**tabdata))
+    if single:
+        dist = generateLD(m, mx)
+    else:
+        dist = generate_two_params(m, d, mx)
+    score = sum(-np.log(np.array(dist) ** tabdata))
 
     return score
-    
-# df1 = pd.read_csv('mutantcount1', header=None)
-# m = np.median(df1)
-# data = df1.to_numpy().flatten()
-# print(score(data, m))
